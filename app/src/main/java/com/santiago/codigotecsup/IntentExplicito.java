9e7b0b7c56c9b235210bb4a4cf7d3f74b0d143f7
Santiago <gerencia@evolutionsoluciones.com>
2019-04-03 21:10:11 -0500

    Comentado intent explicitos

--- app/src/main/java/com/santiago/codigotecsup/IntentExplicito.java
+++ app/src/main/java/com/santiago/codigotecsup/IntentExplicito.java
@@ -8,34 +8,45 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class IntentExplicito extends AppCompatActivity {
-    Button b;
-    EditText t;
+    Button b;// declaramos el boton a usar
+    EditText t;// declaramos el Edit Text a usar
     public static final int TEXT_REQUEST = 1;
+
+    //Se llama al iniciar la aplicación
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intent_explicito);
-        setTitle("Ejemplo Intent Explicito");
-        b = findViewById(R.id.boton);
-        t = findViewById(R.id.texto);
+        super.onCreate(savedInstanceState); // Llamamos al metodo del padre
+        setContentView(R.layout.activity_intent_explicito);// Setteamos layout
+        setTitle("Ejemplo Intent Explicito"); // Cambiamos el titulo
+        b = findViewById(R.id.boton); // Asignamos nuestro boton
+        t = findViewById(R.id.texto); // Asignamos nuestro edit text
 
+        //Asignamos el evento Click a nuestro boton
         b.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) {//Metodo llamado al apretar el boton
+                //Creamos un intent para cambiar de actividad
                 Intent intent = new Intent(getApplicationContext(), IntentExplicitoSegundo.class);
+                // a nuestro intent le agregamos un mensaje sacado de nuestro edittext
                 intent.putExtra("message", t.getText().toString());
+                //
+                //startActivity(intent);
                 startActivityForResult(intent,TEXT_REQUEST);
             }
         });
 
     }
 
+    // Metodo que se llama al retornar de una actividad
     @Override
     public void onActivityResult(int requestCode,
                                  int resultCode, Intent data) {
+        //llamo al metodo del padre
         super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == TEXT_REQUEST) {
             if (resultCode == RESULT_OK) {
+                //Obtengo mi respuesta de un extra
                 String reply =
                         data.getStringExtra("respuesta");
                 t.setText(reply);
--- app/src/main/java/com/santiago/codigotecsup/IntentExplicitoSegundo.java
+++ app/src/main/java/com/santiago/codigotecsup/IntentExplicitoSegundo.java
@@ -8,25 +8,33 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class IntentExplicitoSegundo extends AppCompatActivity {
-
+    TextView textView;
     Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_explicito_segundo);
+        // Obtengo el intent enviado desde la actividad anterior
         Intent intent = getIntent();
-        String message = intent.getStringExtra("message");
-        TextView textView = findViewById(R.id.texto1);
+        // Saco el extra llamano message
+        Bundle bb = intent.getExtras();
+        String message = bb.getString("message");
+
+        textView = findViewById(R.id.texto1);
         textView.setText(message);
 
-        b = findViewById(R.id.back);
 
+        b = findViewById(R.id.back);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Creo un nuevo intent
                 Intent replyIntent = new Intent();
+                // pongo mi respuesta como extra
                 replyIntent.putExtra("respuesta", "respuesta del segundo intent");
+                //setteo el resultado y mi intent
                 setResult(RESULT_OK, replyIntent);
+                // cierro mi actividad
                 finish();
             }
         });
