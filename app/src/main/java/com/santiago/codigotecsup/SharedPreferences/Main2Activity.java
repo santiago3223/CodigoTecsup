1cc6abd99f594fd5b7405596d2f8900255b9bf66
Santiago <gerencia@evolutionsoluciones.com>
2019-05-13 15:04:38 -0500

    Shared preferences

---
+++ app/src/main/java/com/santiago/codigotecsup/SharedPreferences/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.santiago.codigotecsup.SharedPreferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.santiago.codigotecsup.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(
+                "Demo Preferencias", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean("Inicio Sesión",  true);
+        editor.commit();
+
+        boolean logeado = sharedPref.getBoolean("Inicio Sesión",false);
+    }
+}
